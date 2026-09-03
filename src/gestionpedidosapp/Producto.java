faab330b01d79d124a2543d6245e6c8c2b81c0a7
Luis Enrique Valverde Leyva <developerperu2025@gmail.com>
2026-09-02 19:57:21 -0500

    feat(model): creacion de la clase Producto con atributos y metodo mostrarDatos

---
+++ src/gestionpedidosapp/Producto.java
@@ -0,0 +1,41 @@
+package gestionpedidosapp;
+
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+
+    public Producto(int id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("ID: " + id + " | Producto: " + nombre + " | Precio: S/. " + precio);
+    }
+}
